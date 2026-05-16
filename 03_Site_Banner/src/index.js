f0b5de152dd732b8b82f3c34755665a280e030e0
Shraman9011 <shramansm108@gmail.com>
2026-05-16 21:20:35 +0530

    learning how to create site banner

---
+++ 03_Site_Banner/src/index.js
@@ -0,0 +1,38 @@
+import Redis from "ioredis";
+import mongoose from "mongoose";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+const BANNER_KEY = "app:banner";
+
+app.post("/banner", async (req, res) => {
+  await redis.set(
+    BANNER_KEY,
+    req.body?.message || `Welcome to Redis!${req.body?.message}`,
+  );
+  res.json({ success: true });
+});
+
+app.get("/banner", async (req, res) => {
+  const message = await redis.get(BANNER_KEY);
+  res.json({ message });
+});
+
+app.delete("/banner", async (req, res) => {
+  await redis.del(BANNER_KEY);
+  res.json({ success: true });
+});
+
+app.get("/banner/exists", async (req, res) => {
+  const exist = await redis.exists(BANNER_KEY);
+  res.json({ exist: exist });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
